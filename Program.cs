eae4644ec8a52fe113f7145b9de74310a0bb04ef
Pingvu <alka04006@utb.vaxjo.se>
2022-09-29 13:28:04 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Uppgift_3._1
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Hur gammal är du? (år)");
+            int ålder = int.Parse(Console.ReadLine());
+            if (ålder >= 16 && ålder <= 19)
+            {
+                Console.WriteLine("Du får delta i tävlingen");
+            }
+            else
+                Console.WriteLine("Du får inte delta i tävlingen");
+                
+            
+
+        }
+    }
+}
